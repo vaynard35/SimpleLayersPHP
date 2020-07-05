63433bbb52d1600aad233b1cc2677853dc60856e
pawel.kolacinski <pawel.kolacinski@gmail.com>
2020-07-05 14:20:01 +0200

    initial commit

---
+++ SimpleLayersClass.php
@@ -0,0 +1,203 @@
+<?php 
+
+class SimpleLayers {
+
+
+private $layers=array();
+
+private $output; //output resource, it can be helpfull to init class with it to quicken computations
+
+
+function __construct($output=0){
+
+
+$this->output=$output;
+
+}
+
+
+public function addLayer($path,$layerName,$width=0,$height=0,$deltaX=0,$deltaY=0){
+
+
+
+	$this->layers[$layerName]=array(
+	"path"=>$path,
+	"layerName"=>$layerName,
+	"width"=>$width,
+	"height"=>$height,
+	"deltaX"=>$deltaX,
+	"deltaY"=>$deltaY
+	);
+
+
+
+
+}
+
+
+public function removeLayer($layerName){
+
+	unset ($this->layers[$layerName]);
+
+}
+
+
+public function printLayers(){
+
+
+ print("<pre>".print_r($this->layers,true)."</pre>");
+
+
+}
+
+
+
+public function combineLayers(){
+
+
+  
+
+
+
+
+$layers=$this->layers;
+
+$output=$this->output;
+
+$i=0;
+
+
+foreach ($layers as $name=>$layer):
+
+	
+
+    $url=$layer["path"];
+   
+
+
+    $type=$this->getFileExtension($url);
+
+
+
+    
+
+
+    if($type=='jpg') $layerImg=imagecreatefromjpeg($url); 
+    if($type=='png') {$layerImg=imagecreatefrompng($url);imagealphablending($layerImg, true); imagesavealpha($layerImg, true); }
+
+    $width=imagesx($layerImg);
+    $height=imagesy($layerImg);
+
+
+    if($width!=$layer['width'] && $layer['width']!=0 && isset($layer['width']) ) 
+    {
+        $width=$layer['width'];
+        $height=$layer['height'];
+
+        $layerImg=imagescale($layerImg,$width);
+
+
+    }
+
+
+    if(isset($layer['deltaX'])) $deltaX=$layer['deltaX']; else $deltaX=0;
+    if(isset($layer['deltaY'])) $deltaY=$layer['deltaY']; else $deltaY=0;
+
+
+    if($i==0 && !$output) 
+
+    	{  
+    		 $output=$layerImg; 
+
+             
+
+             
+    	} 
+    else 
+    	{
+        
+ 
+
+        
+        imagecopy($output, $layerImg, $deltaX, $deltaY, 0, 0, $width, $height);
+
+
+        
+    	}
+
+
+
+
+
+
+
+
+    
+$i++;
+
+endforeach;
+
+
+$this->output=$output;
+
+
+
+
+}
+
+
+
+public function setOutput($output){
+
+	$this->output=$output;
+}
+
+
+public function getOutput(){
+
+
+	return $this->output;
+
+}
+
+
+
+
+public function saveAsImg($destinationFile){
+
+    
+
+
+    $ext=$this->getFileExtension($destinationFile);
+
+    if($ext=='png') $saveFunction="imagepng"; else $saveFunction="imagejpeg";
+
+
+	if($this->output){$saveFunction($this->output, $destinationFile); return true;} else return false;
+
+
+}
+
+
+
+
+
+function getFileExtension($filename){
+
+    $pathParts=pathinfo($filename);
+
+
+    $ext=strtolower($pathParts["extension"]);
+
+    if($ext=='png') return 'png'; else return 'jpg';
+
+
+}
+
+
+
+
+}
+
+
+
---
+++ test/test_layers.php
@@ -0,0 +1,46 @@
+<?php require  "../SimpleLayersClass.php";
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//simple usage
+
+
+$layers=new SimpleLayers();
+
+$layers->addLayer("coast-1564063_640.jpg","Coast");
+
+$layers->addLayer("circle.png","Circle",0,0,79,1); //we move image by [79,1]pixels from upper left corner
+
+
+$layers->printLayers(); //method shows us layers we have
+
+$layers->combineLayers();//creates Resource object in class that stores our combined image
+
+$layers->saveAsImg("outputImages/output1.jpg");
+
+
+
+//we can pass output from created layers (after combineLayers method) to new layers object
+
+$layers2=new SimpleLayers($layers->getOutput());
+
+$layers2->addLayer("rectangle.png","Rectangle",0,0,0,162);
+
+$layers2->combineLayers();
+
+$layers2->saveAsImg("outputImages/output2.jpg");
+
+
+
+//we can also merge pngs to get png, we also scale our rectangle img to 200x100
+
+$layers3=new SimpleLayers();
+//$layers3->addLayer("coast-1564063_640.jpg","Coast");
+$layers3->addLayer("circle.png","Circle");
+$layers3->addLayer("rectangle.png","Rectangle",200,100,100,150);
+$layers3->combineLayers();
+$layers3->saveAsImg("outputImages/output3.png");
+
