51c9882b0de66767f17902bffe08cdd50678fa69
pawel.kolacinski <pawel.kolacinski@gmail.com>
2021-05-28 17:57:47 +0200

    Code Reformatting

--- SimpleLayersClass.php
+++ SimpleLayersClass.php
@@ -2,19 +2,16 @@
 
 class SimpleLayers {
 
-
 private $layers=array();
 
 private $output; //output resource, it can be helpfull to init class with it to quicken computations
 
+function __construct($output=null){
 
-function __construct($output=0){
 
-
-$this->output=$output;
+    $this->output=$output;
 
 }
-
 
 public function addLayer($path,$layerName,$width=0,$height=0,$deltaX=0,$deltaY=0){
 
@@ -51,9 +48,7 @@ public function removeLayer($layerName){
 
 public function printLayers(){
 
-
- print("<pre>".print_r($this->layers,true)."</pre>");
-
+    print("<pre>".print_r($this->layers,true)."</pre>");
 
 }
 
@@ -62,90 +57,65 @@ public function printLayers(){
 public function combineLayers(){
 
 
-  
+    $layers=$this->layers;
+
+    $output=$this->output;
+
+    $i=0;
 
 
+    foreach ($layers as $name=>$layer):
 
+        
 
-$layers=$this->layers;
-
-$output=$this->output;
-
-$i=0;
-
-
-foreach ($layers as $name=>$layer):
-
-	
-
-    $url=$layer["path"];
-   
-
-
-    $type=$this->getFileExtension($url);
-
-
-
+        $url=$layer["path"];
     
 
 
-    if($type=='jpg') $layerImg=imagecreatefromjpeg($url); 
-    if($type=='png') {$layerImg=imagecreatefrompng($url);imagealphablending($layerImg, true); imagesavealpha($layerImg, true); }
+        $type=$this->getFileExtension($url);
 
-    $width=imagesx($layerImg);
-    $height=imagesy($layerImg);
-
-
-    if($width!=$layer['width'] && $layer['width']!=0 && isset($layer['width']) ) 
-    {
-        $width=$layer['width'];
-        $height=$layer['height'];
-
-        $layerImg=imagescale($layerImg,$width);
-
-
-    }
-
-
-    if(isset($layer['deltaX'])) $deltaX=$layer['deltaX']; else $deltaX=0;
-    if(isset($layer['deltaY'])) $deltaY=$layer['deltaY']; else $deltaY=0;
-
-
-    if($i==0 && !$output) 
-
-    	{  
-    		 $output=$layerImg; 
-
-             
-
-             
-    	} 
-    else 
-    	{
-        
- 
-
-        
-        imagecopy($output, $layerImg, $deltaX, $deltaY, 0, 0, $width, $height);
 
 
         
-    	}
 
 
+        if($type=='jpg') $layerImg=imagecreatefromjpeg($url); 
+        if($type=='png') {$layerImg=imagecreatefrompng($url);imagealphablending($layerImg, true); imagesavealpha($layerImg, true); }
+
+        $width=imagesx($layerImg);
+        $height=imagesy($layerImg);
 
 
+        if($width!=$layer['width'] && $layer['width']!=0 && isset($layer['width']) ) 
+        {
+            $width=$layer['width'];
+            $height=$layer['height'];
+
+            $layerImg=imagescale($layerImg,$width);
 
 
+        }
 
 
-    
-$i++;
-
-endforeach;
+        if(isset($layer['deltaX'])) $deltaX=$layer['deltaX']; else $deltaX=0;
+        if(isset($layer['deltaY'])) $deltaY=$layer['deltaY']; else $deltaY=0;
 
 
-$this->output=$output;
+        if($i==0 && !$output) 
+            {  
+                $output=$layerImg; 
+            } 
+        else 
+            {
+                imagecopy($output, $layerImg, $deltaX, $deltaY, 0, 0, $width, $height);
+            }
+            
+        $i++;
+
+    endforeach;
+
+
+    $this->output=$output;
 
 
 
@@ -172,9 +142,6 @@ public function getOutput(){
 
 public function saveAsImg($destinationFile){
 
-    
-
-
     $ext=$this->getFileExtension($destinationFile);
 
     if($ext=='png') $saveFunction="imagepng"; else $saveFunction="imagejpeg";
@@ -192,8 +159,6 @@ public function saveAsImg($destinationFile){
 function getFileExtension($filename){
 
     $pathParts=pathinfo($filename);
-
-
     $ext=strtolower($pathParts["extension"]);
 
     if($ext=='png') return 'png'; else return 'jpg';
